d21293bb0fc79f66faf9d16f67af39334e92788d
Christian Espinoza <chespinoza@gmail.com>
2018-04-22 22:37:21 +0100

    migration from dep to glide

--- main.go
+++ main.go
@@ -8,7 +8,22 @@ import (
 )
 
 func main() {
-	script := `print("holass")`
+	script := `
+	octet = require'octet'
+	ecdh = require 'ecdh'
+	msg = octet.new(#DATA)
+	msg:string(DATA)
+
+	ed25519 =ecdh.new('ec25519')
+	pk, sk = ed25519:keygen()
+
+	sess = ed25519:session(pk, sk)
+
+	zmsg = ed25519:encrypt(sess, msg)
+
+	decipher = ed25519:decrypt(sess, zmsg)
+	print(decipher:string())
+	`
 	res, err := zenroom.Exec(script, "", "")
 	if err != nil {
 		log.Fatal(err)
